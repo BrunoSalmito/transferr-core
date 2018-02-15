45cfaa9e5eb012cc23b50ccf3566ae9a2af8327f
rafasystec <rafasystec@yahoo.com.br>
2018-02-15 10:48:02 -0300

    Function to restrict the location.

--- transferr-core/src/main/java/br/com/transferr/core/dao/CarDAO.java
+++ transferr-core/src/main/java/br/com/transferr/core/dao/CarDAO.java
@@ -9,10 +9,12 @@ import javax.persistence.TypedQuery;
 import org.springframework.stereotype.Repository;
 
 import br.com.transferr.core.exceptions.ValidationException;
+import br.com.transferr.core.metadata.CoordinatesAmplitude;
 import br.com.transferr.core.metadata.CoordinatesQuadrant;
 import br.com.transferr.core.model.Car;
 import br.com.transferr.core.model.User;
 import br.com.transferr.core.responses.ResponseCarsOnline;
+import br.com.transferr.core.util.CoordinatesUtil;
 
 @Repository
 public class CarDAO extends SuperClassDAO<Car> {
@@ -43,23 +45,26 @@ public class CarDAO extends SuperClassDAO<Car> {
 	//TODO IMPLEMENTS QUADRANT
 	public  List<ResponseCarsOnline> getCarsOnline(CoordinatesQuadrant coordinates){
 		StringBuilder query = new StringBuilder();
-		
+		StringBuilder where = new StringBuilder();
 		query.append("SELECT                          					").append("\n");
-	    query.append("    ' ' as photo,                                     ").append("\n");
-	    query.append("    car.model as model,                               ").append("\n");
-	    query.append("    car.identity as placa,                           ").append("\n");
-	    query.append("    car.color as cor ,                               ").append("\n");
-	    query.append("    driver.name as name_driver,                      ").append("\n");
-	    query.append("    coordinate.latitude as latitude,                ").append("\n");
-	    query.append("    coordinate.longitude as longitude              ").append("\n");
-	    query.append("  from car                                          ").append("\n");
-	    query.append("    inner join driver on                              ").append("\n");
-	    query.append("      car.id_driver = driver.id                         ").append("\n");
-	    query.append("    inner join coordinate on                          ").append("\n");
-	    query.append("      coordinate.id_car = car.id                        ").append("\n");
-	    query.append("  where                                             ").append("\n");
-	    query.append("     car.STATUS <> 2                                  ").append("\n");
-
+	    query.append("    ' ' as photo,                                 ").append("\n");
+	    query.append("    car.model as model,                           ").append("\n");
+	    query.append("    car.identity as placa,                        ").append("\n");
+	    query.append("    car.color as cor ,                            ").append("\n");
+	    query.append("    driver.name as name_driver,                   ").append("\n");
+	    query.append("    coordinate.latitude  AS latitude,             ").append("\n");
+	    query.append("    coordinate.longitude AS longitude             ").append("\n");
+	    query.append("  FROM car                                        ").append("\n");
+	    query.append("    INNER JOIN driver ON                          ").append("\n");
+	    query.append("      car.id_driver = driver.id                   ").append("\n");
+	    query.append("    INNER JOIN coordinate ON                      ").append("\n");
+	    query.append("      coordinate.id_car = car.id                  ").append("\n");
+	    where.append("  WHERE                                           ").append("\n");
+	    where.append("     car.STATUS <> 2                              ").append("\n");
+	    CoordinatesAmplitude amplitude 	= CoordinatesUtil.defineCoordinates(coordinates);
+	    where.append(" AND (cs.longitude BETWEEN ").append(amplitude.getMinLongitude()).append(" AND ").append(amplitude.getMaxLongitude()).append(") ").append(" AND ")
+		 .append(" (cs.latitude  BETWEEN ").append(amplitude.getMinLatitude()) .append(" AND ").append(amplitude.getMaxLatitude()).append(") ");
+	    query.append(where.toString());	
 	   
 	    Query qry = getManager().createNativeQuery(query.toString(), ResponseCarsOnline.class);
 		
