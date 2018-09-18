26db26e61d61f5193ca0c83e7e3644873f305c39
root <root@192.168.1.43>
2018-09-18 17:05:49 -0300

    new fields plain tour

--- transferr-core/src/main/java/br/com/transferr/core/model/PlainTour.java
+++ transferr-core/src/main/java/br/com/transferr/core/model/PlainTour.java
@@ -38,6 +38,21 @@ public class PlainTour  extends Entidade{
 	@org.hibernate.annotations.ColumnDefault("true")
 	private Boolean open = Boolean.TRUE;
 	
+	
+	private String namePassenger1;
+	private String telPassenger1;
+	private String notesPassenger1;
+	
+	private String namePassenger2;
+	private String telPassenger2;
+	private String notesPassenger2;
+	
+	private String namePassenger3;
+	private String telPassenger3;
+	private String notesPassenger3;
+	
+	private String notesOfPlain;
+	
 	public Boolean getOpen() {
 		return open;
 	}
@@ -76,6 +91,86 @@ public class PlainTour  extends Entidade{
 
 	public void setTourOption(TourOption tourOption) {
 		this.tourOption = tourOption;
+	}
+
+	public String getNamePassenger1() {
+		return namePassenger1;
+	}
+
+	public void setNamePassenger1(String namePassenger1) {
+		this.namePassenger1 = namePassenger1;
+	}
+
+	public String getTelPassenger1() {
+		return telPassenger1;
+	}
+
+	public void setTelPassenger1(String telPassenger1) {
+		this.telPassenger1 = telPassenger1;
+	}
+
+	public String getNotesPassenger1() {
+		return notesPassenger1;
+	}
+
+	public void setNotesPassenger1(String notesPassenger1) {
+		this.notesPassenger1 = notesPassenger1;
+	}
+
+	public String getNamePassenger2() {
+		return namePassenger2;
+	}
+
+	public void setNamePassenger2(String namePassenger2) {
+		this.namePassenger2 = namePassenger2;
+	}
+
+	public String getTelPassenger2() {
+		return telPassenger2;
+	}
+
+	public void setTelPassenger2(String telPassenger2) {
+		this.telPassenger2 = telPassenger2;
+	}
+
+	public String getNotesPassenger2() {
+		return notesPassenger2;
+	}
+
+	public void setNotesPassenger2(String notesPassenger2) {
+		this.notesPassenger2 = notesPassenger2;
+	}
+
+	public String getNamePassenger3() {
+		return namePassenger3;
+	}
+
+	public void setNamePassenger3(String namePassenger3) {
+		this.namePassenger3 = namePassenger3;
+	}
+
+	public String getTelPassenger3() {
+		return telPassenger3;
+	}
+
+	public void setTelPassenger3(String telPassenger3) {
+		this.telPassenger3 = telPassenger3;
+	}
+
+	public String getNotesPassenger3() {
+		return notesPassenger3;
+	}
+
+	public void setNotesPassenger3(String notesPassenger3) {
+		this.notesPassenger3 = notesPassenger3;
+	}
+
+	public String getNotesOfPlain() {
+		return notesOfPlain;
+	}
+
+	public void setNotesOfPlain(String notesOfPlain) {
+		this.notesOfPlain = notesOfPlain;
 	}
 
 	
