a8899d641793aa2d54efa29c7cd1ad875980d662
Antonio Rafael S. Rocha <rafasystec@yahoo.com.br>
2020-07-26 08:21:42 -0300

    covenant

--- transferr-core/src/main/java/br/com/transferr/core/role/RoleCovenant.java
+++ transferr-core/src/main/java/br/com/transferr/core/role/RoleCovenant.java
@@ -42,31 +42,4 @@ public class RoleCovenant extends RoleSuperClass<Covenant> {
 	public List<Covenant> findOnlyActiveOnes() {
 		return daoCovenant.findOnlyActiveOnes();
 	}
-	
-	/*
-	  var covenant = Covenant()
-        with(covenant){
-            description = "Covenio Padre Cícero Auto-peças"
-            category = EnumCategory.AUTOPARTS
-            urlLogo = "https://media.licdn.com/dms/image/C4D0BAQGqZFO3y7mE1Q/company-logo_200_200/0?e=2159024400&v=beta&t=Nca71jPdX0aiVPrtyVdlk516Apneb5YwTJrxXoq1qxE"
-            message = "Tudo em auto peças, voce tem 10% de deconto em qualquer compra"
-
-        }
-        var covenant1 = Covenant()
-        with(covenant1){
-            description = "Covenio Bezerra Oliveira"
-            category = EnumCategory.AUTOPARTS
-            urlLogo = "http://bezerraoliveira.com.br/wp/wp-content/uploads/2016/09/logo-bezerra-oliveira-autopecas@2x.png"
-            message = "Tudo em auto peças, voce tem 10% de deconto em qualquer compra"
-        }
-
-        var covenant2 = Covenant()
-        with(covenant2){
-            description = "Chico do caranguejo"
-            category = EnumCategory.FOOD
-            urlLogo = "https://www.chicodocaranguejo.com.br/wp-content/uploads/logo-chico-.png"
-            message = "10% de desconto em pratos a Lacarte e 20% em pratos executivo. Por favor autorizar os clientes primeiro"
-        }
-	 */
-
 }
